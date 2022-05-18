8d91d2cca671ac2e779333d68b0c6a4aaecf34b0
maruf <mmaruf1137@gmail.com>
2022-05-18 21:43:07 +0600

    update

--- src/js/main.js
+++ src/js/main.js
@@ -24,18 +24,53 @@ rangeInputs.forEach((input) => {
 //fuel and tires button active functionality
 const btnFuel = document.querySelectorAll(".btn-fuel");
 
+btnFuel.forEach(function (btnImg) {
+  const btn = btnImg.querySelector(".btn-img");
+  // console.log(btn);
+
+  btn.addEventListener("click", function () {
+    // console.log(btnImg);
+    btnFuel.forEach(function (item) {
+      if (item !== btnImg) {
+        item.classList.remove("active");
+        // console.log("first");
+      }
+    });
+    btnImg.classList.add("active");
+  });
+});
+
+//tires and tires button active functionality
+const btnTires = document.querySelectorAll(".btn-tire");
+
+btnTires.forEach(function (btnImg) {
+  const btn = btnImg.querySelector(".btn-img");
+  console.log(btn);
+
+  btn.addEventListener("click", function () {
+    // console.log(btnImg);
+    btnTires.forEach(function (item) {
+      if (item !== btnImg) {
+        item.classList.remove("active");
+        // console.log("first");
+      }
+    });
+    btnImg.classList.add("active");
+  });
+});
+
 // const handleImageChange = (e) => {
-//   let target = e.target.closest(".btn");
-//   console.log(target);
-// if (target.classlist !== "btn") {
-//   target = target.closest(".btn");
-//   if (target.className === "active") return;
-//   console.log(target.classlist);
-//   target.className += " active";
-// }
-// target.classlist.add("active");
+//   let target = e.target.closest(".fuel-btn");
+//   console.log(e.target.parentElement.className);
 // };
 
 // btnFuel.forEach((e) => {
 //   e.addEventListener("click", handleImageChange);
+// });
+
+// const btnParent = document.querySelector(".fuel");
+
+// btnParent.addEventListener("click", function (e) {
+//   const btnImg = e.target.parentElement;
+//   console.log(btnImg);
 // });
